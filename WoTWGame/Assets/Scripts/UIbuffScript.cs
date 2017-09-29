45878b843e1c4cef27853d50f3720cf37853b5e3
Rikkaboy <mooreman9@gmail.com>
2017-09-29 03:44:13 -0400

    Buff menu triggers set
    Will need to finish Mod tracking to complete

--- WoTWGame/Assets/Scripts/UIbuffScript.cs
+++ WoTWGame/Assets/Scripts/UIbuffScript.cs
@@ -3,39 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIbuffScript : MonoBehaviour {
-    public Texture deerIcon;
-    public Texture wolfIcon;
-    public Texture shrubIcon;
+    public bool ShowShrub;
+    public GameObject shrubGUI;
+    public bool ShowDeer;
+    public GameObject deerGUI;
+    public bool ShowWolf;
+    public GameObject wolfGUI;
 
-    public Rect shrubGUI;
-    public Rect deerGUI;
-    public Rect wolfGUI;
+    public bool shrubGrow;
+    public GameObject shrubGrowGUI;
+    public bool shrubShrink;
+    public GameObject shrubShrinkGUI;
+    public bool shrubTough;
+    public GameObject shrubToughGUI;
+    public bool shrubWeak;
+    public GameObject shrubWeakGUI;
 
-    public Rect shrubSizeGUI;
-    public Rect shrubToughGUI;
-    public Rect deerSizeGUI;
-    public Rect deerToughGUI;
-    public Rect deerSpeedGUI;
-    public Rect wolfSizeGUI;
-    public Rect wolfToughGUI;
-    public Rect wolfSpeedGUI;
+    public bool deerGrow;
+    public GameObject deerGrowGUI;
+    public bool deerShrink;
+    public GameObject deerShrinkGUI;
+    public bool deerTough;
+    public GameObject deerToughGUI;
+    public bool deerWeak;
+    public GameObject deerWeakGUI;
+    public bool deerSpeedUp;
+    public GameObject deerSpeedUpGUI;
+    public bool deerSpeedDown;
+    public GameObject deerSpeedDownGUI;
 
-    public Texture growIcon;
-    public Texture shrinkIcon;
-    public Texture speedUpIcon;
-    public Texture speedDownIcon;
-    public Texture toughIcon;
-    public Texture weakIcon;
-
-    public bool shrubBuffed;
-    public bool deerBuffed;
-    public bool wolfBuffed;
+    public bool wolfGrow;
+    public GameObject wolfGrowGUI;
+    public bool wolfShrink;
+    public GameObject wolfShrinkGUI;
+    public bool wolfTough;
+    public GameObject wolfToughGUI;
+    public bool wolfWeak;
+    public GameObject wolfWeakGUI;
+    public bool wolfSpeedUp;
+    public GameObject wolfSpeedUpGUI;
+    public bool wolfSpeedDown;
+    public GameObject wolfSpeedDownGUI;
     private SimpleEcologyMasterScript eco;
 
     // Use this for initialization
     void Start () {
-
-        
 }
 	
 	// Update is called once per frame
@@ -46,23 +58,35 @@ public class UIbuffScript : MonoBehaviour {
         {
             if(eco.shrubSizeMod >= 1)
             {
-
+                shrubGrow = true;
+                shrubShrink = false;
             }
             else
             {
-
+                shrubShrink = true;
+                shrubGrow = false;
             }
         }
         if(eco.shrubToughMod != 0)
         {
             if(eco.shrubToughMod >= 1)
             {
-
+                shrubTough = true;
+                shrubWeak = false;
             }
             else
             {
-
+                shrubWeak = true;
+                shrubTough = false;
             }
+        }
+        if (eco.shrubSizeMod != 0 || eco.shrubToughMod != 0)
+        {
+            ShowShrub = true;
+        }
+        else
+        {
+            ShowShrub = false;
         }
 
         //checking deer
@@ -70,34 +94,49 @@ public class UIbuffScript : MonoBehaviour {
         {
             if (eco.deerSizeMod >= 1)
             {
-
+                deerGrow = true;
+                deerShrink = false;
             }
             else
             {
-
+                deerShrink = true;
+                deerGrow = false;
             }
         }
         if (eco.deerToughMod != 0)
         {
             if (eco.deerToughMod >= 1)
             {
-
+                deerTough = true;
+                deerWeak = false;
             }
             else
             {
-
+                deerWeak = true;
+                deerTough = false;
             }
         }
         if (eco.deerSpeedMod != 0)
         {
             if (eco.deerSpeedMod >= 1)
             {
-
+                deerSpeedUp = true;
+                deerSpeedDown = false;
             }
             else
             {
-
+                deerSpeedDown = true;
+                deerSpeedUp = false;
             }
+        }
+
+        if(eco.deerSizeMod !=0 || eco.deerToughMod != 0 || eco.deerSpeedMod != 0)
+        {
+            ShowDeer = true;
+        }
+        else
+        {
+            ShowDeer = false;
         }
 
         // checking wolves
@@ -105,55 +144,73 @@ public class UIbuffScript : MonoBehaviour {
         {
             if (eco.wolfSizeMod >= 1)
             {
-
+                wolfGrow = true;
+                wolfShrink = false;
             }
             else
             {
-
+                wolfShrink = true;
+                wolfGrow = false;
             }
         }
         if (eco.wolfToughMod != 0)
         {
             if (eco.wolfToughMod >= 1)
             {
-
+                wolfTough = true;
+                wolfWeak = false;
             }
             else
             {
-
+                wolfWeak = true;
+                wolfTough = false;
             }
         }
         if (eco.wolfSpeedMod != 0)
         {
             if (eco.wolfSpeedMod >= 1)
             {
-
+                wolfSpeedUp = true;
+                wolfSpeedDown = false;
             }
             else
             {
-
+                wolfSpeedDown = true;
+                wolfSpeedUp = false;
             }
         }
-    }
-    private void OnGUI()
-    {
-        //draw species icons
-        GUI.DrawTexture(shrubGUI, shrubIcon);
-        GUI.DrawTexture(deerGUI, deerIcon);
-        GUI.DrawTexture(wolfGUI, wolfIcon);
+        if (eco.wolfSizeMod != 0 || eco.wolfToughMod != 0 || eco.wolfSpeedMod != 0)
+        {
+            ShowWolf = true;
+        }
+        else
+        {
+            ShowWolf = false;
+        }
 
-        //draw buffs: shrub
-        GUI.DrawTexture(shrubSizeGUI, growIcon);
-        GUI.DrawTexture(shrubToughGUI, toughIcon);
+        //Update Icons
+        shrubGUI.SetActive(ShowShrub);
+        deerGUI.SetActive(ShowDeer);
+        wolfGUI.SetActive(ShowWolf);
 
-        //draw buffs: deer
-        GUI.DrawTexture(deerSizeGUI, growIcon);
-        GUI.DrawTexture(deerToughGUI, toughIcon);
-        GUI.DrawTexture(deerSpeedGUI, speedUpIcon);
+        shrubGrowGUI.SetActive(shrubGrow);
+        shrubShrinkGUI.SetActive(shrubShrink);
+        shrubToughGUI.SetActive(shrubTough);
+        shrubWeakGUI.SetActive(shrubWeak);
 
-        //draw buffs: wolves
-        GUI.DrawTexture(wolfSizeGUI, growIcon);
-        GUI.DrawTexture(wolfToughGUI, toughIcon);
-        GUI.DrawTexture(deerSpeedGUI, speedUpIcon);
+        deerGrowGUI.SetActive(deerGrow);
+        deerShrinkGUI.SetActive(deerShrink);
+        deerToughGUI.SetActive(deerTough);
+        deerWeakGUI.SetActive(deerWeak);
+        deerSpeedUpGUI.SetActive(deerSpeedUp);
+        deerSpeedDownGUI.SetActive(deerSpeedDown);
+
+        wolfGrowGUI.SetActive(wolfGrow);
+        wolfShrinkGUI.SetActive(wolfShrink);
+        wolfShrinkGUI.SetActive(wolfShrink);
+        wolfToughGUI.SetActive(wolfTough);
+        wolfWeakGUI.SetActive(wolfWeak);
+        wolfSpeedUpGUI.SetActive(wolfSpeedUp);
+        wolfSpeedDownGUI.SetActive(wolfSpeedDown);
     }
 }
