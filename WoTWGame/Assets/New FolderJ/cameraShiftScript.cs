27b9b3ceeac9d64f09c0cd62fc7cb80d67ad8edc
incarnum <jarhumph>
2017-12-06 05:56:24 -0500

    Giant Tree, Parallax, Misc Environment
    Added everything for the giant tree except for the trunk art
    Converted camera to perspective and adjusted scales and positions of
    everything so stuff doesnt move weirdly
    Expanded map
    Added modular cliffs and dirt paths
    Added Ruins
    Added script for slopes.

---
+++ WoTWGame/Assets/New FolderJ/cameraShiftScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraShiftScript : MonoBehaviour {
+	public bool stage1;
+	public bool stage2;
+	private GameObject player;
+	private Transform target1;
+	public float speed;
+	private float startTime;
+	public float duration;
+	public Animator branch1;
+	public Animator landscape;
+	public Animator branch3;
+	public Animator branch4;
+	public Animator branch5;
+	public Animator branch6;
+	public Animator branch7;
+	public Animator branch8;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+//		target1 = GetComponentsInChildren<Transform>()[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (stage1) {
+//			player.transform.position = Vector3.Lerp(transform.position, target1.position, ((Time.time - startTime) / duration));
+//		}
+	}
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+//			stage1 = true;
+//			startTime = Time.time;
+			Camera.main.GetComponent<Animator>().SetTrigger("TreeCinematic");
+			branch1.SetTrigger ("move7");
+			branch3.SetTrigger ("move7");
+			landscape.SetTrigger ("spin");
+//			branch3.SetTrigger ("move2");
+//			branch4.SetTrigger ("move4");
+//			branch5.SetTrigger ("move5");
+//			branch6.SetTrigger ("move6");
+//			branch7.SetTrigger ("move7");
+//			branch8.SetTrigger ("move8");
+
+			// put it all in one animator
+
+		}
+	}
+}
---
+++ WoTWGame/Assets/New FolderJ/topologyTestScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topologyTestScript : MonoBehaviour {
+	public bool touching;
+	private GameObject player;
+	public int direction;
+	public float intensity;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (touching) {
+			switch( direction) {
+			case 1:
+				float movex = (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + movex, player.transform.position.z);
+				break;
+			case 2:
+				float movex2 = (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + movex2, player.transform.position.z);
+				float movey2 = (Input.GetAxisRaw ("Vertical") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + movey2, player.transform.position.z);
+				break;
+			case 3:
+				float movey3 = (Input.GetAxisRaw ("Vertical") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + movey3, player.transform.position.z);
+				break;
+			case 4:
+				float movex4 = (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - movex4, player.transform.position.z);
+				float movey4 = (Input.GetAxisRaw ("Vertical") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + movey4, player.transform.position.z);
+				break;
+			case 5:
+				float movex5 = (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - movex5, player.transform.position.z);
+				break;
+			case 6:
+				float movex6 = (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - movex6, player.transform.position.z);
+				float movey6 = (Input.GetAxisRaw ("Vertical") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - movey6, player.transform.position.z);
+				break;
+			case 7:
+				float movex7 = (Input.GetAxisRaw ("Vertical") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - movex7, player.transform.position.z);
+				break;
+			case 8:
+				float movex8 = (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + movex8, player.transform.position.z);
+				float movey8 = (Input.GetAxisRaw ("Vertical") * Time.deltaTime * intensity);
+				player.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - movey8, player.transform.position.z);
+				break;
+
+			}
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			touching = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			touching = false;
+		}
+	}
+}
