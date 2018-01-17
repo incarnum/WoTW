3e535e2a8a88005949e54f418eb1cd1589d2c386
incarnum <jarhumph>
2018-01-17 17:14:52 -0500

    Map tweaks
    added some cracked ground, more tree border, more anyimals to fill out
    space

---
+++ WoTWGame/Assets/smoothCamScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothCamScript : MonoBehaviour {
+	public Transform target;
+	public float camDistance;
+	public float smoothTime = 0.3F;
+	private Vector3 velocity = Vector3.zero;
+	void Update() {
+		Vector3 targetPosition = new Vector3(target.position.x, target.position.y, target.position.z + camDistance);
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}
