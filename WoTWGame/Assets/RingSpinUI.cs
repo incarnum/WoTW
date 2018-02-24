b86f4adce60e22aae763ae0c4acfe2d075591197
incarnum <jarhumph>
2018-02-24 02:50:23 -0500

    Main menu work
    Added half polished loading screen work
    Updated Main menu UI, need to adjust the background forest it pans over
    Took another stab at dialogue box art

---
+++ WoTWGame/Assets/RingSpinUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RingSpinUI : MonoBehaviour {
+	public float speed;
+	public float boostSpeed;
+	public float boostDuration;
+	private bool speedBoost;
+	private float boostEnd;
+	private RectTransform rt;
+	// Use this for initialization
+	void Start () {
+		rt = GetComponent<RectTransform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (speedBoost == false) {
+			rt.Rotate (Vector3.forward * Time.deltaTime * speed);
+		} else {
+			rt.Rotate (Vector3.forward * Time.deltaTime * boostSpeed);
+			if (Time.time > boostEnd) {
+				speedBoost = false;
+			}
+		}
+	}
+
+	public void SpeedBoost () {
+		speedBoost = true;
+		boostEnd = Time.time + boostDuration;
+	}
+}
--- WoTWGame/Assets/Scripts/PlayButtonScript.cs
+++ WoTWGame/Assets/Scripts/PlayButtonScript.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButtonScript : MonoBehaviour {
-
+	public RingSpinUI ring;
+	public GameObject screenFade;
 	// Use this for initialization
 	void Start () {
 		Time.timeScale = 1f;
+
 	}
 	
 	// Update is called once per frame
@@ -16,6 +19,35 @@ public class PlayButtonScript : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-		SceneManager.LoadScene ("Forest 2");
+		
+		StartCoroutine (LoadAsynchronously());
+		StartCoroutine (SayFrameALot());
 	}
+
+	public void Pressed() {
+		StartCoroutine (LoadAsynchronously());
+		ring.SpeedBoost ();
+	}
+
+	IEnumerator LoadAsynchronously ()
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync ("Forest 2");
+		Application.backgroundLoadingPriority = ThreadPriority.Low;
+		while (!operation.isDone) {
+			Debug.Log (operation.progress);
+			if (operation.progress >= .9f) {
+				screenFade.SetActive (true);
+				yield return new WaitForSeconds (1);
+			}
+			yield return null;
+		}
+	}
+
+	IEnumerator SayFrameALot()
+	{
+		print ("frame");
+		Application.backgroundLoadingPriority = ThreadPriority.High;
+		yield return null;
+	}
+		
 }
